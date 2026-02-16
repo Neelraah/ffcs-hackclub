eed2ac79d26cf80a72938f88cac2fc8ae79257cc
Anas Arfeen <codecrusader07@gmail.com>
2026-02-16 21:48:08 +0530

    -Sample dataset generated using general format of json file -Removed data visualisation

--- AI/Dataset/app.py
+++ AI/Dataset/app.py
@@ -1,25 +1,23 @@
-from flask import Flask, render_template
-import requests
-
-app = Flask(__name__)
-
-URL = "https://vitverse.itzdivyanshupatel.workers.dev/faculty-ratings"
-
-@app.route("/")
-def index():
-    try:
-        response = requests.get(URL, timeout=5)
-        data = response.json()
-        faculty_list = data.get("data", [])
-    except Exception as e:
-        print("Fetch failed:", e)
-        # fallback to local JSON
-        import json
-        with open("faculty_data.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-            faculty_list = data.get("data", [])
-
-    return render_template("index.html", faculty=faculty_list)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# json = {
+#     faculy_id,
+#     faculy_name,
+#     total_rating,
+#     avg_teaching,
+#     avg_attendance_flex,
+#     avg_supportiveness,
+#     avg_marks,
+#     avg_overall,
+#     courses(dict)->{
+#         code,
+#         title
+#     },
+#     comments(dict)->{
+#         review,
+#         course(dict)->{
+#             code,
+#             title
+#         },
+#         student_name,
+#         submitted_at
+#     }
+# }
